97556405b140157139d7ba4d008c71d0ed0cb0b4
Axis <arunbluez@gmail.com>
2017-12-26 20:59:48 +0100

    get clients

--- models/redis.js
+++ models/redis.js
@@ -2,7 +2,7 @@ var redisClient = require('redis').createClient;
 var redis = redisClient(6379, '127.0.0.1');
 
 var channelName;
-
+var clients;
 
 redis.auth("tfuncredispass");
 module.exports.publish = function(channel, message){
@@ -11,7 +11,9 @@ module.exports.publish = function(channel, message){
 }
 
 function intervalFunc() {
+  redis.client("list", clients);
   redis.publish(channelName,"status:1");
+  console.log(clients);
 }
 
 setInterval(intervalFunc, 1500);
