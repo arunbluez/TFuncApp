13a50f179ae9049712ead91025c2270159f0c057
Axis <arunbluez@gmail.com>
2017-12-26 17:12:56 +0100

    publish to channel test

--- models/redis.js
+++ models/redis.js
@@ -1,13 +1,17 @@
 var redisClient = require('redis').createClient;
 var redis = redisClient(6379, '127.0.0.1');
 
+var chanelName;
+
+
 redis.auth("tfuncredispass");
 module.exports.publish = function(channel, message){
+  chanelName = channel;
     redis.publish(channel,message);
 }
 
 function intervalFunc() {
-  console.log('Cant stop me now!');
+  redis.publish(chanelName,"Test";
 }
 
 setInterval(intervalFunc, 1500);
