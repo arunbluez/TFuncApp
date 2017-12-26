be26018709415310e577bc536e13371aaf7ea726
Axis <arunbluez@gmail.com>
2017-12-26 15:22:41 +0100

    try timed loop

--- models/redis.js
+++ models/redis.js
@@ -5,3 +5,9 @@ redis.auth("tfuncredispass");
 module.exports.publish = function(channel, message){
     redis.publish(channel,message);
 }
+
+function intervalFunc() {
+  console.log('Cant stop me now!');
+}
+
+setInterval(intervalFunc, 1500);
