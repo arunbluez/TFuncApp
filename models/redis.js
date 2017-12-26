f0fe5f8f93c6e4f9917597a7d8af3facb4f9c6f3
Axis <arunbluez@gmail.com>
2017-12-26 21:24:34 +0100

    get client

--- models/redis.js
+++ models/redis.js
@@ -3,6 +3,7 @@ var redis = redisClient(6379, '127.0.0.1');
 
 var channelName;
 var clients;
+var indices;
 
 redis.auth("tfuncredispass");
 module.exports.publish = function(channel, message){
@@ -13,9 +14,31 @@ module.exports.publish = function(channel, message){
 function intervalFunc() {
   redis.client("list", function (err, value){
     if (err) throw(err)
-    console.log(value)
+    indices = getIndicesOf("name", value);
 })
+  for (index in indices){
+    if(value.substring(index + 5, index + 6) != ""){
+      console.log(value.substring(index + 5, index + 23));
+    }
+  }
   redis.publish(channelName,"status:1");
 }
 
 setInterval(intervalFunc, 1500);
+
+function getIndicesOf(searchStr, str) {
+    var searchStrLen = searchStr.length;
+    if (searchStrLen == 0) {
+        return [];
+    }
+    var startIndex = 0, index, indices = [];
+
+    str = str.toLowerCase();
+    searchStr = searchStr.toLowerCase();
+
+    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+        indices.push(index);
+        startIndex = index + searchStrLen;
+    }
+    return indices;
+}
